0996879bbd4c83d38a95287d357aeaa4c3164848
DarthLillith <Conrade290@yahoo.com>
2023-04-11 22:54:57 -0500

    BuildingPuzzle
    Added building blocks
    A button that moves a block to a goal point
    Script to check if it hit goal point
    Script to make button function with sound.

---
+++ KolbeVR/Assets/Scripts/Button/ButtonVR.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonVR : MonoBehaviour
+{
+    public GameObject button;
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+    GameObject presser;
+    AudioSource sound;
+   public bool isPressed;
+
+
+ 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        isPressed = false;
+        
+    }
+
+  
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isPressed)
+        {
+            button.transform.localPosition = new Vector3(0, 0.003f, 0);
+            presser = other.gameObject;
+            onPress.Invoke();
+            sound.Play();
+            isPressed = true;
+           
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == presser)
+        {
+            button.transform.localPosition = new Vector3(0, 0.015f, 0);
+            onRelease.Invoke();
+            isPressed = false; 
+
+        }
+    }
+
+
+    
+
+}
---
+++ KolbeVR/Assets/Scripts/Button/MovingCube.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour
+{
+
+    public GameObject CubetobeMoved;
+    public float speed;
+    public Transform Target;
+    public ButtonVR buttonVr;
+    public GameObject Endzone;
+
+    public bool endZoneReached;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonVr.isPressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCube();
+
+    }
+
+    public void MoveCube()
+    {
+        if (buttonVr.isPressed == true)
+        {
+            float step = speed * Time.deltaTime;
+            CubetobeMoved.transform.position = Vector3.MoveTowards(transform.position, Target.position, step);
+        }
+    }
+
+    private void GameOver()
+    {
+        if (CubetobeMoved.transform.position == Endzone.transform.position)
+        {
+            // Give reward/key Congratulations
+
+            Debug.Log("congratulations");
+        }
+
+
+    }
+
+
+}
+
