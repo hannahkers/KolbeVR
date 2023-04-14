bdf7e1fd71bc3b2f674e23f39db24db3a840361c
Hannah Stern <92752125+hannahkers@users.noreply.github.com>
2023-04-14 10:12:09 -0500

    Scratch Lines

--- KolbeVR/Assets/Scripts/GameMenuManager.cs
+++ KolbeVR/Assets/Scripts/GameMenuManager.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class GameMenuManager : MonoBehaviour
 {
-    
+
+    public void Update()
+    {
+        if (Input.anyKey)
+        {
+            Debug.Log(Input.inputString);
+        }
+    }
+
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+
     }
 
 }
